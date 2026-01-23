e9b9ec237a25baeedb978bbce0ac37105b6ab099
eliselorraine <el@skalar.no>
2026-01-23 07:14:26 +0100

    merge function needs debugging

--- day_five/src/main.rs
+++ day_five/src/main.rs
@@ -4,24 +4,39 @@ use std::path::{Path, PathBuf};
 
 fn main() {
     let input_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("puzzle_input.txt");
-    let (ranges, ids) = read_input(input_path);
-    let mut fresh_ids = Vec::new();
-    for (idx, id) in ids.into_iter().enumerate() {
-        for range in &ranges {
-            if range.contains(&id) {
-                fresh_ids.push(idx); 
-            }
-        }    
+    let (mut ranges, _ids) = read_input(input_path);
+    ranges.sort_by_key(|r| *r.start());
+    let merged = merge(&ranges);
+    let mut count: u64 = 0;
+    for range in merged {
+        count += (range.end() - range.start()) + 1;
     }
 
-    fresh_ids.dedup();
-    println!("{:?}", fresh_ids.len());
+    println!("{:?}", count);
 }
 
-fn read_input(path: PathBuf) -> (Vec<RangeInclusive<usize>>, Vec<usize>) {
+fn merge(sorted_ranges: &Vec<RangeInclusive<u64>>) -> Vec<RangeInclusive<u64>> {
+    let mut merged: Vec<RangeInclusive<u64>> = Vec::new();
+    let mut current: RangeInclusive<u64> = sorted_ranges[0].to_owned();
+    for range in sorted_ranges {
+        if range.start() < current.end() {
+            let new_range = current.start()..=range.end();
+            current = *current.start()..=*range.end();
+            continue
+        } 
+
+        merged.push(current);
+        current = range.clone();
+    }
+    
+    merged.push(current);
+    merged
+}
+
+fn read_input(path: PathBuf) -> (Vec<RangeInclusive<u64>>, Vec<u64>) {
     let content: String = fs::read_to_string(path).expect("Couldn't read file.");
-    let mut ranges: Vec<RangeInclusive<usize>> = Vec::new();
-    let mut ids: Vec<usize> = Vec::new();
+    let mut ranges: Vec<RangeInclusive<u64>> = Vec::new();
+    let mut ids: Vec<u64> = Vec::new();
 
     for line in content.lines() {
         if line.is_empty() {
@@ -30,11 +45,11 @@ fn read_input(path: PathBuf) -> (Vec<RangeInclusive<usize>>, Vec<usize>) {
     
         if let Some((start, end)) = line.split_once('-') {
             ranges.push(
-                start.parse::<usize>().unwrap()
-                    ..= end.parse::<usize>().unwrap(),
+                start.parse::<u64>().unwrap()
+                    ..= end.parse::<u64>().unwrap(),
             );
         } else {
-            ids.push(line.parse::<usize>().unwrap());
+            ids.push(line.parse::<u64>().unwrap());
         }
     }
 
@@ -46,11 +61,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn range_one() -> Result<(), Box<dyn std::error::Error>> {
-        let input: RangeInclusive<usize> = 3..=5;
-        let id: usize = 1;
-        let result = check_id(input, 1);
-        assert_eq!(false, result);
+    fn merge_ranges() -> Result<(), Box<dyn std::error::Error>> {
+        let ranges: Vec<RangeInclusive<u64>> = vec![3..=5, 4..=6, 10..=20];
+        let result = merge(&ranges);
+        assert_eq!(vec![3..=6, 10..=20], result);
         Ok(())
     }
 }
